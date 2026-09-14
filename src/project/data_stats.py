82e322cec0e29871eaf34c1c14b69733a8008f49
IdaPug <s204211@dtu.dk>
2026-09-14 21:32:07 +0200

    Add CML data statistics report

--- src/project/data_stats.py
+++ src/project/data_stats.py
@@ -1,23 +1,30 @@
+import os
+
 import matplotlib.pyplot as plt
 from data import DisasterTweetData
 
 
 def data_statistics(datadir: str = "data"):
+    os.makedirs("reports/figures", exist_ok=True)
+
     out_report = "reports/report.md"
 
     train_data = DisasterTweetData(data_path=f"{datadir}/train.csv")
     val_data = DisasterTweetData(data_path=f"{datadir}/valid.csv")
 
-    # write the number of samples in the training and validation datasets to a report
+    # Write dataset statistics to report
     with open(out_report, "w") as f:
         f.write("# Data Statistics Report\n\n")
-        f.write("## Training Dataset\n")
+
+        f.write("## Training Dataset\n\n")
         f.write(f"Number of samples: {len(train_data)}\n\n")
-        f.write("## Validation Dataset\n")
+
+        f.write("## Validation Dataset\n\n")
         f.write(f"Number of samples: {len(val_data)}\n\n")
 
-    # plot how much of each class is in the training dataset
+    # Plot training class distribution
     train_labels = train_data.data["target"].tolist()
+
     plt.pie(
         [train_labels.count(0), train_labels.count(1)],
         labels=["Not Disaster", "Disaster"],
@@ -28,8 +35,9 @@ def data_statistics(datadir: str = "data"):
     plt.savefig("reports/figures/train_class_distribution_pie.png")
     plt.close()
 
-    # plot how much of each class is in the validation dataset
+    # Plot validation class distribution
     val_labels = val_data.data["target"].tolist()
+
     plt.pie(
         [val_labels.count(0), val_labels.count(1)],
         labels=["Not Disaster", "Disaster"],
@@ -39,6 +47,14 @@ def data_statistics(datadir: str = "data"):
     plt.title("Validation Dataset Class Distribution")
     plt.savefig("reports/figures/val_class_distribution_pie.png")
     plt.close()
+
+    # Add plots to the report
+    with open(out_report, "a") as f:
+        f.write("## Training Class Distribution\n\n")
+        f.write("![Training class distribution]" "(figures/train_class_distribution_pie.png)\n\n")
+
+        f.write("## Validation Class Distribution\n\n")
+        f.write("![Validation class distribution]" "(figures/val_class_distribution_pie.png)\n\n")
 
 
 if __name__ == "__main__":
